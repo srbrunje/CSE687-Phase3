0cf45c2b5f3d1e18e31e4bcc2ed135938ed545d8
Leo Garza <leog4za@gmail.com>
2021-05-25 09:55:57 -0500

    Update TestClass.h

--- CSE687_Phase3/TestClass.h
+++ CSE687_Phase3/TestClass.h
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef __TEST_CLASS_H
+#define __TEST_CLASS_H
+
 
 /***********************************************
 * CSE678 Object Oriented Design
@@ -12,63 +14,149 @@
 *
 * File: TestClass.h
 *
-* Data Defined: class TestResult
+* Data Defined: class TestClass
 *
 * Description: Implements a class to handle the
-*              result of a test.
+*              creation and manipulation of a
+*			   single test.
 *
 ***********************************************/
 
 #include <string>
-#include <json/json.h>
+#include <functional>
+
 #include "utils.h"		// common utility functions and constants
+#include "TestResult.h"	// TestResult class
 
 
-/* Class: TestResult
-* Contains basic data describing the result of the test to include pass/fail status,
-*	any error messages, test duration, and the desired log level
+
+/* Class: TestClass
+* Enables the ability to handle the creation and manipulation of a single test.
 */
-class TestResult {
+class TestClass {
 public:
-	// Public enum for TestResult's current status
-	enum class Status {
-		PASS, FAIL, Fail_EXC, NOT_RUN
-	};
+
+	typedef std::function<bool()> CallableObject;
+
+	/*************************************************************************
+	*
+	* Default constructor for the TestClass class
+	*
+	* Parameter: 	None
+	*
+	* return:	None
+	*
+	*************************************************************************/
+	TestClass();
+
+	// Custom Constructor
+	TestClass(CallableObject aTestMethod,
+		LogLevel aLogLevel,
+		std::string aName = "", std::string customErrorMessage = "Default Error Message: The test did not pass. Something wrong must have occurred");
+
+	/*************************************************************************
+	*
+	* Setter for the test name
+	*
+	* Parameter: 	name: The name of the test
+	*
+	* return:	None
+	*
+	*************************************************************************/
+	void SetName(std::string name);
+
+	/*************************************************************************
+	*
+	* Getter for the test name
+	*
+	* Parameter: 	none
+	*
+	* return:	The name of the test
+	*
+	*************************************************************************/
+	std::string GetName() const;
+
+	/*************************************************************************
+	*
+	* Setter for the test method refrence
+	*
+	* Parameter: 	method: a refrence to the test that will be run
+	*
+	* return:	None
+	*
+	*************************************************************************/
+	void SetTestMethod(CallableObject method);
+
+	/*************************************************************************
+	*
+	* Getter for the test method refrence
+	*
+	* Parameter: 	None
+	*
+	* return:	a refrence to the test that will be run
+	*
+	*************************************************************************/
+	CallableObject GetTestMethod() const;
+
+	/*************************************************************************
+	*
+	* Setter for the log level
+	*
+	* Parameter: 	level: the level of logging for this test
+	*
+	* return:	None
+	*
+	*************************************************************************/
+	void SetLogLevel(LogLevel level);
+
+
+
+
+	/*************************************************************************
+	*
+	* Getter for the log level
+	*
+	* Parameter: 	None
+	*
+	* return:	the level of logging for this test
+	*
+	*************************************************************************/
+	LogLevel GetLogLevel() const;
+
+	///*************************************************************************
+	//*
+	//* Setter for the result of the test
+	//*
+	//* Parameter: 	result: pass or fail, or not run (default)
+	//*
+	//* return:	None
+	//*
+	//*************************************************************************/
+	//void SetTestResult(TestResult result);
+
+	// Replacing the above with "RunTest()" rather than having an outside source
+	//   determine and set the result, this class can run it internally
+	const TestResult* RunTest();
+
+
+	/*************************************************************************
+	*
+	* Getter for the result of the test
+	*
+	* Parameter: 	None
+	*
+	* return:	pass or fail or not run
+	*
+	*************************************************************************/
+	TestResult GetTestResult() const;
 
 private:
+	std::string _name;
+	std::string _errorMessage;
+	CallableObject _testMethod;
+	LogLevel _logLevel;
+	TestResult _testResult;
 
-	// Private Member Variables (PMV)
-	std::string  _name;			// name of the test that resulted in this result
-	Status       _status;		// pass, fail, or not run status denotation
-	std::string  _errorMessage;	// any/all error messages that occurred during test
-	timing::hack _startTime;	// the time the test began to execute
-	timing::hack _endTime;		// the time the test finished execution
-	LogLevel     _logLevel;		// the level of logging desired
-	Json::Value root; // JSON ROOT {}
-	Json::Value data; // JSON DATA 'NAME' = '', 
-
-	// Private Member Functions (PMF)
-	// - none for now
-
-public:
-	// Constructors
-	TestResult(const std::string& aName = "");	// default constructor with option of setting name
-
-
-	// Setters
-	void SetName(const std::string& aName);
-	void SetStatus(const Status aStatus);
-	void SetErrorMessage(const std::string& aErrMsg);
-	void SetStartTime(const timing::hack aStartTime);
-	void SetEndTime(const timing::hack aEndTime);
-	void SetLogLevel(const LogLevel aLogLevel);
-	void SetClassObject();
-
-	// Getters
-	std::string     GetName() const;
-	Status          GetStatus() const;
-	std::string     GetErrorMessage() const;
-	double          GetDuration() const;	// returns the time taken to run the test in microseconds
-	LogLevel		GetLogLevel() const;
-	std::string		GetClassObject();
 };
+
+#endif // !__TEST_CLASS_H
